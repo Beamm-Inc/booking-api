9d485916570c1f54d3340c5f82f788bbed0ebeaf
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-03-31 11:45:19 -0500

    add docker

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# booking-api
+## Flight Booking API
+
+### Installation
+
+Install `maven` and `docker`
+
+`mvn clean install`
+
+`cd src/main/docker`
+
+`docker-compose up`
--- src/main/java/com/beamm/flightbooking/bootstrap/BootstrapData.java
+++ src/main/java/com/beamm/flightbooking/bootstrap/BootstrapData.java
@@ -1,11 +1,9 @@
 package com.beamm.flightbooking.bootstrap;
 
-import com.beamm.flightbooking.controller.AirplaneController;
 import com.beamm.flightbooking.model.Airplane;
 import com.beamm.flightbooking.repository.AirplaneRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
@@ -19,27 +17,27 @@ public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-//        System.out.println("Loading Airplanes");
-//
-//        Airplane c1 = new Airplane();
-//
-//        c1.setAirplaneModel("Ethiopian");
-//        c1.setAirplaneSerialNumber("kcds");
-//        c1.setBusinessClassSeats(14);
-//        c1.setEconomyClassSeats(73);
-//        c1.setFirstClassSeats(0);
-//        airplaneRepository.save(c1);
-//
-//        Airplane c2 = new Airplane();
-//
-//        c2.setAirplaneModel("Ethiopian");
-//        c2.setAirplaneSerialNumber("kcds");
-//        c2.setBusinessClassSeats(14);
-//        c2.setEconomyClassSeats(73);
-//        c2.setFirstClassSeats(0);
-//        airplaneRepository.save(c2);
-//
-//
-//        System.out.println("Total Airplanes: " + airplaneRepository.count());
+        System.out.println("Loading Airplanes");
+
+        Airplane c1 = new Airplane();
+
+        c1.setAirplaneModel("Ethiopian");
+        c1.setAirplaneSerialNumber("kcds");
+        c1.setBusinessClassSeats(14);
+        c1.setEconomyClassSeats(73);
+        c1.setFirstClassSeats(0);
+        airplaneRepository.save(c1);
+
+        Airplane c2 = new Airplane();
+
+        c2.setAirplaneModel("Ethiopian");
+        c2.setAirplaneSerialNumber("kcds");
+        c2.setBusinessClassSeats(14);
+        c2.setEconomyClassSeats(73);
+        c2.setFirstClassSeats(0);
+        airplaneRepository.save(c2);
+
+
+        System.out.println("Total Airplanes: " + airplaneRepository.count());
     }
 }

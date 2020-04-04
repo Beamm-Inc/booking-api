22d44d65261f1af0c38f079312f30ea6f3bd295a
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-04-04 18:26:53 -0500

    add missing rest controller

--- src/main/java/com/beamm/flightbooking/controller/ScheduledFlightController.java
+++ src/main/java/com/beamm/flightbooking/controller/ScheduledFlightController.java
@@ -6,16 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.Iterator;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(ScheduledFlightController.BASE_URL)
 public class ScheduledFlightController {
     @Autowired

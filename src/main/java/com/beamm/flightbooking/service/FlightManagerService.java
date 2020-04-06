da940e1ebb1cf738c5a77d891f43b3827a0c97ff
Amanuel Negussie Kihshen <62534492+akihshen@users.noreply.github.com>
2020-04-06 00:56:30 -0500

    full implentation of flight manager

--- src/main/java/com/beamm/flightbooking/service/FlightManagerService.java
+++ src/main/java/com/beamm/flightbooking/service/FlightManagerService.java
@@ -3,6 +3,7 @@ package com.beamm.flightbooking.service;
 import com.beamm.flightbooking.model.FlightManager;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -10,7 +11,7 @@ public interface FlightManagerService {
     Page<FlightManager> getAllFlightManagerPages(int pageNo);
     List<FlightManager> getAllFlightManagers();
     FlightManager getFlightManagerById(Integer id);
-//    FlightManager getFlightManagerByUserNameAndPassword(String userName, String password);
-    HttpStatus deleteFlightManagerById(Integer id);
-    FlightManager saveFlightManager(FlightManager flightManager);
+    FlightManager getFlightManagerByUserNameAndPassword(String userName, String password);
+    ResponseEntity<FlightManager> deleteFlightManagerById(Integer id);
+    FlightManager addFlightManager(FlightManager flightManager);
 }
